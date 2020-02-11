3beb44a69093f32b83f31af01fd9378a26957b09
Antonio Alvarado <ant.36alv@gmail.com>
2020-02-11 12:27:04 -0500

    Have several questions, some styling done. Bug with presentQuestion.

--- script.js
+++ script.js
@@ -2,29 +2,72 @@
 const maxTime = 75;
 var highScore;
 var seconds;
+var questionsAnswered = 0;
 // var seconds = maxTime;
-
-var currentQuestion;
-
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var currentQuestion = 0;
 var startBtn = document.getElementById("startButton");
 
 //displays
 var timeLeft = document.getElementById("displayTime");
 var questionDisplay = document.getElementById("displayQuestion");
 
+var rightDisplay = document.getElementById("displayRight");
+rightDisplay.textContent = `Correct Answers: ${correctAnswers}`;
+
+var wrongDisplay = document.getElementById("displayWrong");
+wrongDisplay.textContent = `Wrong Answers: ${wrongAnswers}`;
+
+var timer;
 var answersDiv = document.getElementById("answersDiv");
 
+
 var questions = [];
-
+var highScorers = [];
 //first answer is always the right answer, however will be displayed in a random order
-var firstQuestion = {
-    question : "What does UFC stand for?",
-    answers: ["0", "1", "2", "3"],//["Ultimate Fighting Championship", "Universal fun cup", "U Fork Carrots", "Unified Flamingo Conference"],
-    /*  TODO change answers back*/
-    answered : false
-};
+// // var firstQuestion = {
+//     question : "What does UFC stand for?",
+//     answers: ["Ultimate Fighting Championship", "Universal fun cup", "U Fork Carrots", "Unified Flamingo Conference"],
+// };
 
-questions.push(firstQuestion);
+questions.push(firstQuestion = {
+    question : "What does UFC stand for?",
+    answers: ["Ultimate Fighting Championship", "Universal fun cup", "U Fork Carrots", "Unified Flamingo Conference"],
+});
+//I'll always remember this as the night that Michael Jordan and I combined for 70 points.
+
+questions.push(secondQuestion = { 
+    question : "Stacky King once said, \"I'll always remember this as the night that Michael Jordan\n and I combined for 70 points\"." +
+    "How many points did he score that night?", 
+    answers: ["1", "42", "17", "25"]
+});
+
+questions.push(thirdQuestion = {
+    question : "NFL \n Recieving yards after the age of 40\nJerry Rice : 2509\nBrett Farve : "+ 
+                "?\nEveryone else : 0\nHow yards does Brett Farve have?",
+    answers : ["-2 yards", "47 yards", "9 yards", "15 yards"]
+});
+
+questions.push(fourthQuestion = {
+    question : "What is the most points scored in an NBA Finals Game?",
+    answers : ["61 points", "42 points", "55 points", "29 points", "77 points"]
+});
+
+questions.push(fifthQuestion = {
+    question : "Brazil is the country with the most World Cups, how many do they have?",
+    answers : ["5", "3", "2", "7"]
+});
+
+questions.push(sixthQuestion = {
+    question : "Jamie Moyer is the oldest MLB pitcher to start and win a World Series game. What team did he play for?",
+    answers : ["Phillies", "Dodgers", "Yankees", "Cardinals"]
+});
+
+// questions.push(secondQuestion={question : "wtf right now", answers: ["0", "1", "2", "3"]} );
+var questionsRotation = shuffle(questions);
+
+
 
 //probably maybe till further notice (well till I think of something better)
 
@@ -37,15 +80,13 @@ questions.push(firstQuestion);
 //answered : false
 //whether it has been answered or not
 
-//I want 4 answers, one will be the right one
 
 //TODO may or may not be a good idea to create an object for every player
 //thats obtains a high score (max 3 players)
 
-// var highScorePlayer = {
-//     name;
+var highScorePlayer = {
 
-// };
+};
 
 //starts quiz, starts the timer at 60 seconds and counts down.
 startBtn.addEventListener("click", function() {
@@ -58,18 +99,31 @@ startBtn.addEventListener("click", function() {
 });
 
 function startGame() {
+    currentQuestion = 0;
     displayTime(maxTime);
-    setInterval(function() {
+    timer = setInterval(function() {
+        // clearInterval();
         if(seconds > 0) {
             seconds--;
             displayTime(seconds);
+            //TODO no time end game condition
+        } else {
+            alert("game over");
+            resetGame();
         }
     }, 1000);
-    presentQuestion();
+    presentQuestion(questionsRotation[currentQuestion]);
 }
 
 function resetGame() {
-    displayTime(maxTime);
+
+    questionsAnswered.textContent = (questionsAnswered = 0);
+    rightDisplay.textContent = (correctAnswers = 0);
+    wrongDisplay.textContent = (wrongAnswers = 0);
+    questionsRotation = shuffle(questions);
+    currentQuestion = randomNumber(questions.length);
+    clearInterval(timer);
+    startGame();
 }
 
 function displayTime(time){
@@ -77,36 +131,62 @@ function displayTime(time){
     timeLeft.textContent = time;
 }
 //function presentQuestion(element)
-function presentQuestion(){
+function presentQuestion(questionIndex){
+    console.log(questionIndex);
+    let presentQ = questionsRotation[questionIndex];
     //random number for picking a question to present
-    let randomIndex = randomNumber(questions.length);
+    // currentQuestion = questionIndex;
+    console.log(presentQ);
+    questionDisplay.textContent = questions[presentQ].question;
 
-    questionDisplay.textContent = questions[randomIndex].question;
+    // presentQuestion(questionsRotation[currentQuestion + 1]);
+
     //we have a question presented now
     //present the answers in a random order
     //will return an array 0-n but in a random order
     //n = the lenght of questions
-    let randomOrderArray = shuffle(questions[randomIndex].answers);//randomOrder(questions[randomIndex].answers);
+    let answersRotation = shuffle(questions[presentQ].answers);
 
-    for(i = 0; i < (questions[randomIndex].answers.length); i++){
-            
+    while(answersDiv.firstChild) {
+        //https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+        answersDiv.removeChild(answersDiv.firstChild);
+    }
+
+    for(i = 0; i < (questions[presentQ].answers.length); i++){
+        
         let answerBtn = document.createElement("button");
-        answerBtn.textContent = questions[randomIndex].answers[randomOrderArray[i]];
-        answerBtn.setAttribute("value", questions[randomIndex].answers.indexOf(answerBtn.textContent));
+        answerBtn.textContent = questions[presentQ].answers[answersRotation[i]];
+        answerBtn.setAttribute("value", questions[presentQ].answers.indexOf(answerBtn.textContent));
         answersDiv.appendChild(answerBtn);
     }
 
     for(i = 0; i < answersDiv.childElementCount; i++) {
-        //console.log(answersDiv.children[i].value)
-        answersDiv.children[i].addEventListener("click", print)
+        answersDiv.children[i].addEventListener("click", checkAnswer)
     }
 }
 
-function print(e) {
-
+function checkAnswer(event) {
     if(event.target.value==0) {
-        // console.log("value 0");
+        correctAnswers+=1;
+        rightDisplay.textContent = correctAnswers;
+
+        //TODO change to something more appropiate, aka not an alert
+    } else {
+        wrongAnswers+=1;
+        wrongDisplay.textContent = wrongAnswers;
+        let timePenalty = seconds - 5;
+        displayTime(timePenalty);
+        //TODO call displayTime function and subtract time 
     }
+
+    questionsAnswered++;
+    if(questionsAnswered === questions.length) {
+        console.log("alll answered reseting")
+        resetGame();
+    }
+    console.log(questionsRotation[currentQuestion]);
+    // console.log(questionsRotation[currentQuestion + 1]);
+    presentQuestion(questionsRotation[currentQuestion+=1]);
 }
 
 function randomNumber(max) {
@@ -116,22 +196,16 @@ function randomNumber(max) {
 function shuffle(array) {
 
     let shuffledArray = [];
-
     //TODO comment this code
     var control= -1;//control variable - need better name
-    console.log("before")
-    console.log(array)
     while (shuffledArray.length < array.length) {
         let rando = randomNumber(array.length);
 
         //if shuffledArray already has a random index (0...array.length)
         if(shuffledArray.includes(rando)){
             let ind = shuffledArray.indexOf(rando);
-            // console.log();
-            // shuffledArray.push(rando);
             shuffledArray.splice(ind, 1, rando);
             
-            //  test--;
         } else {
             shuffledArray.push(rando);
                 if(control<array.length-1) {
@@ -139,11 +213,5 @@ function shuffle(array) {
                 }
         }
     }
-    console.log("after")
-    console.log(shuffledArray)
     return shuffledArray;
 }
-
-// function checkAnswer(e) {
-//     // console.log(e.)+
-// }
